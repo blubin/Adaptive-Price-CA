636fd0858d0828726fc5695f2f951c46a3ecf6ce
blubin <lubin@post.harvard.edu>
2024-09-12 15:35:14 -0400

    Update run_pytest.sh

--- experiments/run_pytest.sh
+++ experiments/run_pytest.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# A script to run one of our experiments in python
-
-# First Make sure the commands are ok in a machine-specific way:
-if [ $HOSTNAME == "SOMETHING" ]; then
-    function pytest() { /usr/local/bin/py.test $@; }
-fi
+source ./common.sh
 
 pytest $@
