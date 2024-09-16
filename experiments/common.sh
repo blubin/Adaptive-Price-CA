b069b7722b902db6e365dd08fb52472a6c2c6a2c
Ben Lubin <blubin@bu.edu>
2024-09-16 16:30:09 -0400

    update to add pytest content

--- experiments/common.sh
+++ experiments/common.sh
@@ -3,12 +3,22 @@
 # First make sure the commands are ok in a OS specific way:
 if [[ "$OSTYPE" == "cygwin" ]]; then
     function python() { /cygdrive/c/Python27/python.exe $@; }
+    function pytest() { /cygdrive/c/Python27/Scripts/pytest.exe $@; }
 else
     function python() { /usr/local/bin/python2.7 $@; }
+    function pytest() {
+	#Attempt to extrapolate where pytest is from python location:
+	pytest=/usr/local/bin/python2.7  # Provide python path
+	pytest=`readlink -n $pytest`     # Expand symlink to true path
+	pytest="$(dirname $pytest)"      # Get the directory
+	pytest=$pytest/Scripts/pytest    # append the subpath to pytest
+	$pytest $@;                      # execute
+    }
 fi
 
 # Next you can override this if you need to in a machine specific way:
 # Edit the below if you need to...
 if [ $HOSTNAME == "SOMETHING" ]; then
-    function python() { /usr/local/bin/python2.7 $@; }
+    function python() { /path/to/python2.7 $@; }
+    function pytest() { /path/to/pytest $@; }
 fi
